f0fb7a00daed61e2159452900569a1d274ba79a6
Kody <im@kdy.ch>
2023-11-22 13:49:00 +0100

    And fix base path on example's build

--- example/vite.config.js
+++ example/vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   server: {
     fs: {
       // Allow serving files from one level up to the project root
